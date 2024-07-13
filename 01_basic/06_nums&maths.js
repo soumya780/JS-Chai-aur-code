99b800e816d9ed9dbc3c8486ceb47f2bbf39c06d
soumya780 <soumyapanda780@gmail.com>
2024-07-13 12:28:14 +0530

    30 days challenge day 1

--- 01_basic/06_nums&maths.js
+++ 01_basic/06_nums&maths.js
@@ -1,7 +1,36 @@
 const score = 6969
-
-const balance = new Number (12)
+console.log(score);
+const balance = new Number(223)
 console.log(balance);
-console.log(typeof(score));
-console.log(typeof(balance));
-djjdj
+// console.log(typeof(score));
+// console.log(typeof(balance));
+console.log(balance.toFixed(2));
+
+const otherNumber = 1123.8778
+//.toPrecision(5) precises the number till 5 th place 
+// 1122.689
+// 1234 567 .toPrecision(5) => 1122.7
+console.log(otherNumber.toPrecision(5));
+
+// const hundreds = 100000000
+// console.log(hundreds.toLocaleString());
+// // Output : 100,000,000
+// // This provides the USA standards 
+
+//To use the Indian Standards 
+// console.log(Rupee.toLocaleString('en-IN'));
+// ('en-In') it provides the Indian Standard
+const Rupee = 10000000
+console.log(Rupee.toLocaleString('en-IN'));
+//output: 1,00,00,000
+
+//************* Math ********** */
+
+console.log(Math);
+console.log(Math.abs(-5));
+console.log(Matha.round(-5.998));
+console.log(Matha.round(-5.998));
+
+
+
+
---
+++ 30daysJSChallenge/day1.js
@@ -0,0 +1,87 @@
+//* Activity 1 */
+var variable = 22
+console.log(variable);
+
+let variableTwo = "soumya "
+console.log(variableTwo);
+
+//* Activity 2 *
+
+const result = true;
+console.log(result);
+
+//* Activity 3 */
+
+let num = 22
+let name = "Sonu"
+let obj = {
+    GameName: "Pubg",
+    mobile: "iphone 14"
+}
+game = obj.GameName
+let arr = ["mummi", "papa", "sohan"]
+
+console.table([num, name, typeof (game), arr])
+// console.log(game);
+
+// console.log("num =" + typeof(num));
+// console.log("name =" +typeof(name));
+// console.log("obj =" +typeof(obj));
+// console.log("game =" +typeof(game));
+// console.log("arr =" +typeof(arr));
+
+console.table(["num = " + typeof (num), "name = " + typeof (name), "obj = " + typeof (obj), "obj.gameName = " + typeof (game), "array = " + typeof (arr)])
+
+//* Activity 4  */
+
+let aValue = 2233
+console.log(aValue);
+// let newVal = aValue 
+aValue = 229
+console.log(aValue);
+
+// * Activity 5*
+
+// const id = 403
+// id = 203
+// console.log(id);
+
+// Output : Error : Assignment to constant variable
+
+
+// * Feature1 : Variable Types Console Log*
+
+let num1 = 33
+let location = "Bangalore"
+let work = null
+let company
+let big = 3432455n
+let symb = Symbol("233")
+
+let array = ["Sonu", "Soumya"]
+
+let newObj = {
+    phone:"7978112145"
+}
+function data(scName) {
+    console.log(`My School name is ${scName}`
+);    
+}
+
+console.table([typeof(num1),typeof(location),typeof(work),typeof(company),typeof(big),typeof(symb),typeof(array),typeof(newObj),typeof(data)])
+console.log(data("Maa Saraswati Bidya Mandir."))
+console.log(typeof (company));
+
+
+//* Activity 2  */
+
+let sclId = 22
+
+sclId = 33
+
+console.log(sclId);
+const clgId = 33
+
+// You cant change the value of a const datatype.
+
+//Completed the Challenge 1.
