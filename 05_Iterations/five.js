88f61f36a76c2c0287996d2770d8f9b6eda7abd5
soumya780 <soumyapanda780@gmail.com>
2024-07-22 03:15:44 +0530

    final 40 min left

---
+++ 05_Iterations/five.js
@@ -0,0 +1,42 @@
+const coding = ["js","cpp","java","css"]
+// coding.forEach(function(val){
+//     console.log(val);
+// }) its a way to use for each
+
+// coding.forEach( (item) => {
+//     console.log(item);
+// } )
+
+// or else we can first create a function and use that
+
+function printMe (item){
+    // console.log(item);
+}
+
+coding.forEach(printMe)
+
+// coding.forEach( (item , index ,arr )=>{
+//     console.log(item , index, arr);
+// } )
+
+// Another way for for each
+
+
+const code = [
+    {
+        languageName : "java",
+        fileName : ".java"
+    },
+    {
+        languageName : "javascript",
+        fileName : ".js"
+    },
+    {
+        languageName : "react",
+        fileName : ".jsx"
+    }
+]
+
+code.forEach( (item) => {
+    console.log(`Language is ${item.languageName} & its format is ${item.fileName}`);
+} )
--- 05_Iterations/four.js
+++ 05_Iterations/four.js
@@ -1,4 +1,36 @@
-const myObj = {
-    'game1': "Call Of Duty",
-    'game2': "Vice City"
+const myObject = {
+    js: "JavaScript",
+    cpp: "C++",
+    jsx: "React"
+
 }
+
+// to acess the object through loop we use 
+
+for (const key in myObject) {
+    //    console.log(`${key} fullform is ${myObject[key]}`);
+}
+
+let arr = [1, 34, 56, 32, 2, 3]
+
+for (const key in arr) {
+    //   console.log(key); // it will print the index of the arrays 
+    // output : 0 1 2 3 4 5
+
+    // To print the element of the array :
+
+    // console.log(`the ${key} element is : ${arr[key]} `);
+}
+
+
+// npw lets try for the map 
+
+const map = new Map()
+map.set('Game1', "Nfs")
+map.set('Game2', "GTAV")
+map.set('Game3', "COD")
+
+// for (const k in map) {
+//     console.log(k);
+// }
+// We cant iterate the map.
