56e55da5ff127b476c988ae2b791f8c876bc0096
soumya780 <soumyapanda780@gmail.com>
2024-07-27 23:19:13 +0530

    project 1complete

--- 7_project/color changer/chaiaurcode.js
+++ 7_project/color changer/chaiaurcode.js
@@ -1,4 +1,62 @@
-const button =document.querySelectorAll('button')
-const body =document.querySelector("body")
+const buttons = document.querySelectorAll('.button')
+const body = document.querySelector('body')
 
-button.forEach( (button) => console.log(button) )
+buttons.forEach(function (button) {
+    console.log(button)
+    button.addEventListener('click', function (e) {
+        // console.log(e);
+        // console.log(e.target);
+        const color = e.target.id;
+        switch (color) {
+            case "grey":
+
+                body.style.backgroundColor = color
+                button.style.borderColor = "white"
+                break;
+
+            case "white":
+
+                body.style.backgroundColor = color
+                button.style.borderColor = "blue"
+                break;
+
+            case "blue":
+                body.style.backgroundColor = "blue"
+                button.style.borderColor = "yellow"
+                // body.style.color = "white"
+                break;
+            case "yellow":
+
+                body.style.backgroundColor = "yellow"
+                button.style.borderColor = "purple"
+                break;
+            case "purple":
+
+                body.style.backgroundColor = "purple"
+                button.style.borderColor = "#212121"
+                break;
+            case "black":
+
+                body.style.backgroundColor = color
+                button.style.borderColor = "grey"
+                break;
+
+            default:
+                body.style.backgroundColor = "white"
+                button.style.borderColor = color
+
+                break;
+        }
+
+        if (body.style.backgroundColor === "blue" || body.style.backgroundColor === "black" || body.style.backgroundColor === "grey" || body.style.backgroundColor === "purple") {
+            body.style.color = "white" 
+        } else {
+            body.style.color = "black"
+        }
+        // if (e.target.id == "grey") {
+        //     body.style.backgroundColor = "grey"
+        //     // body.style.backgroundColor = e.target.id
+        //     //  we can use this also
+        // }
+    })
+})
